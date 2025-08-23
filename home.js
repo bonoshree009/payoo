c844698aceaed52a3222bcbe1de99e274b8fbb40
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-24 02:18:30 +0600

    cash out section

--- home.js
+++ home.js
@@ -1,12 +1,11 @@
 console.log("connected")
+ const pin = 1234
 document.getElementById("add-money-btn").addEventListener("click",function(){
     //console.log("cliced")
     const addamount = parseInt(document.getElementById("input-addamount").value)
     const preamount = parseInt(document.getElementById("pre-amount").innerText)
     const accountnum = document.getElementById("actnum").value
     // console.log(addamount,preamount)
-
-   const pin = 1234
    const bankpin = parseInt( document.getElementById("bank-pin").value  )
 //   console.log(bankpin)
    if(pin == bankpin && (accountnum.length === 11)){
@@ -18,3 +17,23 @@ document.getElementById("add-money-btn").addEventListener("click",function(){
     alert("Your pin number is wrong")
    }
 })
+ //cash out section
+document.getElementById("cashout-btn").addEventListener("click",function(){
+    //console.log("cliced")
+    const withdraw_amount = parseInt(document.getElementById("amount").value)
+    const preamount = parseInt(document.getElementById("pre-amount").innerText)
+    const agentnum = document.getElementById("agent-number").value
+    // console.log(addamount,preamount)
+   const bankpin = parseInt( document.getElementById("bank-pin").value  )
+//   console.log(bankpin)
+   if(pin == bankpin && (agentnum.length === 11)){
+    const after_withdraw = preamount - withdraw_amount
+   document.getElementById("pre-amount").innerText = after_withdraw
+   console.log("withdraw money")
+   }
+   else{
+    alert("Your pin number is wrong")
+   }
+})
+
+//toggol section
