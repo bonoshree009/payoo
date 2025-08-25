2ae90d9b7850a1e69c4a57fdb3fb1d96287808ef
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-08-25 22:21:07 +0600

    transaction section

--- home.js
+++ home.js
@@ -20,10 +20,10 @@ document.getElementById("add-money-btn").addEventListener("click",function(){
    const addamount = convertnum("input-addamount")
     const preamount = innertextnum("pre-amount")
     const accountnum = onlyValueToNum("actnum")
-    console.log(addamount,preamount)
+    //console.log(addamount,preamount)
    const bankpin = convertnum("bank-pin")
       //console.log(bankpin)
-   if(pin == bankpin && (accountnum.length === 11)){
+   if(pin == bankpin && (accountnum.length === 2)){
     const total = addamount + preamount
    document.getElementById("pre-amount").innerText = total
    console.log("add the money")
@@ -31,6 +31,14 @@ document.getElementById("add-money-btn").addEventListener("click",function(){
    else{
     alert("Your pin number is wrong")
    }
+
+   const data = {
+      name : "Add Money",
+      date : new Date().toLocaleTimeString()
+   }
+
+   transactiondata.push(data)
+   //console.log(transactiondata)
 })
 
 
@@ -43,7 +51,7 @@ document.getElementById("cashout-btn").addEventListener("click",function(){
    console.log(withdraw_amount,preamount)
    const bankpin = convertnum("bank-pin")
   // console.log(bankpin)
-   if(pin == bankpin && (agentnum.length === 1)){
+   if(pin == bankpin && (agentnum.length === 2)){
     const after_withdraw = preamount - withdraw_amount
    document.getElementById("pre-amount").innerText = after_withdraw
    console.log("withdraw money")
@@ -51,12 +59,53 @@ document.getElementById("cashout-btn").addEventListener("click",function(){
    else{
     alert("Your pin number is wrong")
    }
+
+   const data = {
+      name : "cash out",
+      date : new Date().toLocaleTimeString()
+   }
+
+   transactiondata.push(data)
+})
+
+const transactiondata = []
+// transaction btn
+document.getElementById("transaction").addEventListener("click",function(){
+         
+    const transactionContainer = document.getElementById("cart-cotainer")
+    transactiondata.innerText = ""
+    for(const data of transactiondata){
+      const div = document.createElement("div")
+      div.innerHTML = `
+                  <div class=" w-lg mx-auto  my-4 flex items-center justify-between bg-white p-4 rounded-2xl" >
+                       <div class="flex items-center gap-2">
+                             <div class="rounded-full p-2 bg-[#d4d4d4c1]"><img src="assets/wallet1.png" alt=""></div>
+                             <div>
+                              <h1 class="font-bold text-[#080808c1]">${data.name}</h1>
+                              <h3 class="text-[#6f6a6ac1]">${data.date}</h3>
+                             </div>
+                       </div>
+                       <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                 </div>
+      `
+      transactionContainer.appendChild(div)
+    }
 })
 
 
-// transfer btn
+// transaction korar jonno prothome transaction er vitore akta cart design kore nia .Asbe
+// akta array nilam . protita button a akta Object
+//  create kore sei array te rakhlam.akhon transaction btn k get elelmentById dia event
+//  add korte hobe . transaction cart a akta outer div nibo ..sob new div tar moddhe Asbe .
+//  akhon add addEventListener er moddhe sei div er id k access kori getelementBtId dia,
+// akhon for loop dibo sei arrayr opr. vitore kaj hobe akta div create kora and sei div er innerhtml
+//  hisebe sei prothome banano cart er html ta templete literels er moddhe dia. just name ar time k dynamic kora.
+// sei div k puro transaction outer div a append kora . lastly sei outer div ar innertext a akta empty 
+// string rakha
 
-//toggol section
+
+
+//toggol function
        function toggol(id){
          const forms = document.getElementsByClassName("form-parent")
             for(const form of forms){
@@ -84,16 +133,9 @@ document.getElementById("cashout-btn").addEventListener("click",function(){
 
 
   document.getElementById("addMoney").addEventListener("click",function(){
-      //    const forms = document.getElementsByClassName("form-parent")
-      // for(const form of forms){
-      //   form.style.display = "none"
-      // }
-      // document.getElementById("addmoney-div").style.display = "block"
+      
        toggol("addmoney-div")
        hoberButton("addMoney")
-       // console.log("add money button clicked")
-         //   document.getElementById("addMoney").style.backgroundColor = "green"
-         //    document.getElementById("cashOut").style.backgroundColor = "white"
 
   })
    document.getElementById("cashOut").addEventListener("click",function(){
@@ -101,13 +143,6 @@ document.getElementById("cashout-btn").addEventListener("click",function(){
         toggol("cashout-div")
         console.log("cash out button clicked")
          hoberButton("cashOut")
-
-
-          // console.log("cash out button clicked")
-            // document.getElementById("addMoney").style.backgroundColor = "white"
-            // document.getElementById("cashOut").style.backgroundColor = "green"
-
-
   })
 
   document.getElementById("transferMoney").addEventListener("click",function(){
